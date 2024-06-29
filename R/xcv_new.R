79891418a1025f3c76b378347e34d32e92b608bc
cfjdoedens <git@chance-matters.nl>
2024-06-29 13:30:24 +0200

    next commit

--- R/xcv_new.R
+++ R/xcv_new.R
@@ -1,3 +1,15 @@
+#' Split a string by a given character
+#'
+#' @param x     A character vector
+#' @param pattern A regular expression, as used in the stringr package. The default is ",".
+#'          The pattern is used to describe where to split x.
+#' @param n This determines the maximum length of each element of the output.
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' xcv_new("a,b,c", ",")
 xcv_new <- function(x, pattern, n = Inf) {
   stopifnot(is.character(x), length(x) <= 1L)
   if (length(x) == 1L) {
